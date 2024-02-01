e7c288c0abe89f8ad66661df55cfb9d9ef23cb7e
awaik <awaiking44@gmail.com>
2024-02-01 22:28:18 +0700

    init

--- bin/dart_update_locales_fastlane.dart
+++ bin/dart_update_locales_fastlane.dart
@@ -8,7 +8,6 @@ Future<void> main(List<String> arguments) async {
   final parser = ArgParser()
     ..addFlag(kTranslationsDir)
     ..addFlag(kFastlaneDir)
-    ..addFlag(kFlavor)
     ..addFlag(kOS);
 
   ArgResults argResults = parser.parse(arguments);
--- lib/updater/update.dart
+++ lib/updater/update.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 
 import 'package:args/args.dart';
 
-
 ///
 /// Directory with translations made by slang-gpt
 /// https://pub.dev/packages/slang_gpt
@@ -54,12 +53,22 @@ void update(ArgResults argResults) async {
       if (os == 'ios') {
         fileNameTranslatedString = _fileNameTranslatedStringIos;
       } else {
-        fileNameTranslatedString = _fileNameTranslatedStringAndroid;
+        fileNameTranslatedString = {};
+        // fileNameTranslatedString = _fileNameTranslatedStringAndroid;
       }
 
       for (final filename in fileNameTranslatedString.keys) {
         try {
-          String translatedStr = translationsMap[fileNameTranslatedString[filename]];
+          // suggest that translation has structrued like this:
+          // {
+          // "appstore": {
+          //   "name": "Learn English With SpeakFly AI",
+          //   "subtitle": "Tutor - lessons and flashcards",
+          //   "keywords": "flashcards, language, ai, learn language, english tutor, ai tutor, learn english",
+          //   "description": "Master English, German, Spanish, French and 9 more languages effortlessly with SpeakFly! Choose from exciting topics like art, design, philosophy, and cooking for engaging lessons. Get instant feedback on grammar and pronunciation, and complete advanced homework assignments. Let SpeakFly make you a fluent language superstar! \n1. English AI tutor with voice recognition. \n2. Accelerated language learning through example phrases. \n3. The app generates phrases for your words. \n4. Check pronunciation using the built-in reader. \nLicensed Application End User License Agreement \nhttps://ludreamity.com/agreement/ \nTerms of Use \nhttps://www.apple.com/legal/internet-services/itunes/dev/stdeula/"
+          // },
+          String translatedStr =
+              translationsMap[os == 'ios' ? 'appstore' : 'playstore'][fileNameTranslatedString[filename]];
           await _writeToFile(content: translatedStr, path: '$dirPath/$filename');
         } catch (e) {
           print(
@@ -123,27 +132,27 @@ void _printErrors(ErrorType type) {
 
 // available codes  https://docs.fastlane.tools/actions/deliver/
 final Map<String, String> _dirNameFileNameIos = {
-  'default': 'app_en.arb',
-  'ar-SA': 'app_ar.arb',
-  'cs': 'app_cs.arb',
-  'de-DE': 'app_de.arb',
-  'en-US': 'app_en.arb',
-  'es-ES': 'app_es.arb',
-  'fr-FR': 'app_fr.arb',
-  'hu': 'app_hu.arb',
-  'it': 'app_it.arb',
-  'ja': 'app_ja.arb',
-  'ko': 'app_ko.arb',
-  'nl-NL': 'app_nl.arb',
-  'pl': 'app_pl.arb',
-  'pt-BR': 'app_pt-br.arb',
-  'ro': 'app_ro.arb',
-  'ru': 'app_ru.arb',
-  'sk': 'app_sk.arb',
-  'sv': 'app_sv.arb',
-  'tr': 'app_tr.arb',
-  'zh-Hans': 'app_zh-cn.arb',
-  'zh-Hant': 'app_zh-tw.arb',
+  'default': 'translations.i18n.json',
+  'ar-SA': 'translations_ar.i18n.json',
+  // 'cs': 'app_cs.arb',
+  'de-DE': 'translations_de.i18n.json',
+  'en-US': 'translations.i18n.json',
+  'es-ES': 'translations_es.i18n.json',
+  'fr-FR': 'translations_fr.i18n.json',
+  // 'hu': 'app_hu.arb',
+  // 'it': 'translations_it.i18n.json',
+  'ja': 'translations_jp.i18n.json',
+  // 'ko': 'app_ko.arb',
+  // 'nl-NL': 'app_nl.arb',
+  // 'pl': 'app_pl.arb',
+  'pt-PT': 'translations_pt.i18n.json',
+  // 'ro': 'app_ro.arb',
+  'ru': 'translations_ru.i18n.json',
+  // 'sk': 'app_sk.arb',
+  // 'sv': 'app_sv.arb',
+  'tr': 'translations_tr.i18n.json',
+  'zh-Hans': 'translations_cn.i18n.json',
+  // 'zh-Hant': 'app_zh-tw.arb',
 };
 
 // can't find available codes, issue https://github.com/fastlane/fastlane/issues/21011
@@ -168,4 +177,11 @@ final Map<String, String> _dirNameFileNameAndroid = {
   'tr-TR': 'app_tr.arb',
   // 'zh_Hans': 'app_zh-cn.arb',
   'zh-TW': 'app_zh-tw.arb',
+};
+
+final Map<String, String> _fileNameTranslatedStringIos = {
+  'description.txt': "description",
+  'name.txt': "name",
+  'subtitle.txt': "subtitle",
+  'keywords.txt': "keywords",
 };
